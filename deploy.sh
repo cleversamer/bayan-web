dd728001b0bed4d7dd351d872f11f06fd8e25d1f
cleversamerr <thedev.samer@gmail.com>
2023-05-04 16:42:05 +0300

    fixed bugs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Building app..."
-npm run build
+# npm run build
 
 echo "Deploying files to server..."
-scp -r build/* root@191.101.229.249:/var/www/191.101.229.249/
+scp -r build/* samer@191.101.229.249:/var/www/191.101.229.249/
 
 echo "Done!"
--- src/v3/api/client.js
+++ src/v3/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://191.101.229.249:4000/api",
+  baseURL: "http://191.101.229.249/api",
   // baseURL: "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
