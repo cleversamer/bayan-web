5b75d927b6ffcce8c771981c1f35ff3ff086340d
cleversamerr <thedev.samer@gmail.com>
2023-05-07 01:18:24 +0300

    updated server url

--- src/v3/api/client.js
+++ src/v3/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://191.101.229.249/api",
   // baseURL: "http://localhost:4000/api",
-  baseURL: "https://monkeyroadcar.app/api",
+  baseURL: "https://bayanedu.tech/api",
   headers: {
     "Content-Type": "application/json",
   },
